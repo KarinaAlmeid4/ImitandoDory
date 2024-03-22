10d6ebca563ddb8d79a62e7df5013881dd438de8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-22 19:31:47 -0300

    tarefa imitando dory realizada

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("tradutor de baleês");
+Console.Write("Digite o texto a ser traduzido: ");
+
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nA Dory diri...\n");
+
+Console.WriteLine(texto
+.Replace("a" , "aaa")
+.Replace("e" , "eee")
+.Replace("i" , "iii")
+.Replace("o" , "ooo")
+.Replace("u" , "uuu")
+.Replace("A" , "AAA")
+.Replace("E" , "EEE")
+.Replace("I" , "III")
+.Replace("O" , "OOO")
+.Replace("U" , "UUU")
+);
+Console.WriteLine();
